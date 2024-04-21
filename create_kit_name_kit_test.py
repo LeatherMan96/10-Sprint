82283e2f49d9b6cbc18bd92042948393b1e37632
LeatherMan6969 <unface4@gmail.com>
2024-04-21 20:14:04 +0300

    project

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -21,5 +21,32 @@ def positive_assert(name):
     assert kit_response.json()["name"] == {'name': name}
 
 
-def test_create_kit_1_letter():
+def negative_assert(name):
+    kit_body = get_kit_body(name)
+    authToken = get_new_user_token()
+    kit_response = sender_stand_request.post_new_client_kit(kit_body, authToken)
+    assert kit_response.status_code == 400
+
+
+def test_create_kit_1_letters():
     positive_assert("a")
+
+
+def test_create_kit_511_letters():
+    positive_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
+
+
+def test_create_kit_0_letters():
+    negative_assert("")
+
+
+def test_create_kit_512_letters():
+    negative_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
+
+
+def test_create_kit_eng_letters():
+    positive_assert("QWErty")
+
+
+
+def test_create_kit_eng_letters():
